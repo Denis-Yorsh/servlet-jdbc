18a6f836d05f0ee939920a191cbd5bdb84940799
Denis Yorsh <denis_my_mail@yahoo.com>
2022-09-04 19:47:09 +0300

    update class PersonInfo class PersonData

--- src/main/java/com/java/servlet/jdbc/Pojo/PersonData.java
+++ src/main/java/com/java/servlet/jdbc/Pojo/PersonData.java
@@ -8,8 +8,92 @@ public class PersonData {
     private String firstName;
     private String lastName;
     private Integer age;
+    private PersonInfo personInfo;
     private Date date;
     private Integer count;
     private Boolean isDelete;
 
+    public PersonData() {
+    }
+
+    public PersonData(String firstName, String lastName, Integer age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public PersonData(String firstName, String lastName, Integer age, PersonInfo personInfo) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.personInfo = personInfo;
+    }
+
+    public Integer getPersonId() {
+        return personId;
+    }
+    public void setPersonId(Integer personId) {
+        this.personId = personId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public PersonInfo getPersonInfo() {
+        return personInfo;
+    }
+    public void setPersonInfo(PersonInfo personInfo) {
+        this.personInfo = personInfo;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    public Boolean getDelete() {
+        return isDelete;
+    }
+    public void setDelete(Boolean delete) {
+        isDelete = delete;
+    }
+
+    @Override
+    public String toString() {
+        return "Person Data [personId = " + personId +
+                ", First Name = " + firstName +
+                ", Last Name = " + lastName +
+                ", age = " + age +
+                ", personInfo = " + personInfo +
+                ", date = " + date +
+                ", count = " + count +
+                ", isDelete = " + isDelete + "]";
+    }
 }
--- src/main/java/com/java/servlet/jdbc/Pojo/PersonInfo.java
+++ src/main/java/com/java/servlet/jdbc/Pojo/PersonInfo.java
@@ -7,4 +7,47 @@ public class PersonInfo {
     private String phone;
     private String email;
 
+    public PersonInfo() {}
+
+    public PersonInfo(String country, String phone, String email) {
+        this.country = country;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public Integer getInfoId() {
+        return infoId;
+    }
+    public void setInfoId(Integer infoId) {
+        this.infoId = infoId;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Person Info [infoId = " + infoId +
+                ", country = " + country +
+                ", phone = " + phone +
+                ", email = " + email + "]";
+    }
 }
